02d55bd33dd7a69e2d6a7c1e8222bd5381191264
Brent Unkrich <bunkrich@gmail.com>
2017-05-18 11:12:11 -0400

    added migrations

--- db/migrate/20170518103945_create_questions.rb
+++ db/migrate/20170518103945_create_questions.rb
@@ -1,4 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table    :questions do |t|
+      t.string      :body, null: false
+
+      t.references  :user, null: false
+      t.references  :answer
+
+      t.timestamps  null: false
+    end
   end
 end
--- db/migrate/20170518104009_create_comments.rb
+++ db/migrate/20170518104009_create_comments.rb
@@ -1,4 +1,15 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table    :comments do |t|
+      t.string      :body, null: false
+
+      t.references  :user, null: false
+
+      # polymorphic
+      t.integer     :commentable_id, null: false
+      t.string      :commentable_type, null: false
+
+      t.timestamps  null: false
+    end
   end
 end
