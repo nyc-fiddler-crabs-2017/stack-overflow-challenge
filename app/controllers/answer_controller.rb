96cf0345f82f2e61251055b465c46f61d92dad5c
Alex Camacho <email": null>
2017-05-18 16:38:44 -0400

    Create basic answers routes, only /new has erb

---
+++ app/controllers/answer_controller.rb
@@ -0,0 +1,12 @@
+get '/answers' do
+  "This is the answers route"
+end
+
+get '/questions/:id/answers/new' do
+  @question = Question.find_by(id: params[:id])
+  erb :'/answers/new'
+end
+
+post '/answers' do
+  "This is the /answers post route"
+end
