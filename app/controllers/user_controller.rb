4d039f35f25ad74baa84e38aaf49f32570cdc4b4
Brent Unkrich <bunkrich@gmail.com>
2017-05-18 15:46:43 -0400

    built out user routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,44 @@
+get '/login' do
+  erb :'users/login'
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:user][:username])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["Email and Password do not match our records."]
+    erb :'users/login'
+  end
+end
+
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    status 400
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  require_user
+  @user = User.find_by(id: params[:id])
+  if require_owner(@user.id)
+    erb :'users/show'
+  else
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
