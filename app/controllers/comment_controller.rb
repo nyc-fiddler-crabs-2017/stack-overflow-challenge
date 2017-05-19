5b63f6306d52d731106cc6244c0540e10e9255f9
Brent Unkrich <bunkrich@gmail.com>
2017-05-19 10:27:21 -0400

    updated routes for comment_controller

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,49 @@
+get '/questions/:question_id/comments' do
+  @question = Question.find(params[:question_id])
+  @comments = @question.comments
+  erb :'comments/index'
+end
+
+get '/questions/:question_id/comments/new' do
+  @question = Question.find(params[:question_id])
+  erb :'comments/new'
+end
+
+post '/questions/:question_id/comments' do
+  @question = Question.find(params[:question_id])
+  @comment = @question.comments.new(params[:comment])
+  if @comment.save
+    redirect "/questions/#{@question.id}/comments"
+  else
+    erb :'comments/new'
+  end
+end
+
+# get '/questions/:question_id/comments/:id' do
+#   @question = Question.find(params[:question_id])
+#   @comment = @question.comments.find(params[:id])
+#   erb :'comments/show'
+# end
+
+get '/questions/:question_id/comments/:id/edit' do
+  @question = Question.find(params[:question_id])
+  @comment = @question.comments.find(params[:id])
+  erb :'comments/edit'
+end
+
+put '/questions/:question_id/comments/:id' do
+  @question = Question.find(params[:question_id])
+  @comment = @question.comments.find(params[:id])
+  if @comment.update_attributes(params[:comment])
+    redirect "/questions/#{@question.id}/comments"
+  else
+    erb :'comments/edit'
+  end
+end
+
+delete '/questions/:question_id/comments/:id' do
+  @question = Question.find(params[:question_id])
+  @comment = @question.comments.find(params[:id])
+  @comment.destroy
+  redirect "/questions/#{@question.id}/comments"
+end
