8183e2663cdc74f3c394a46e38bca01035fc75e2
Alex Camacho <email": null>
2017-05-19 10:33:07 -0400

    Complete edit answer route and view

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -2,18 +2,35 @@ get '/answers' do
   "This is the answers route"
 end
 
-get '/questions/:id/answers/new' do
+get '/questions/:question_id/answers/new' do
   @question = Question.find_by(id: params[:id])
   erb :'/answers/new'
 end
 
-post '/questions/:id/answers' do
-  @question = Question.find_by(id: params[:id])
-  @answer = Answer.new(body: params[:body], question_id: params[:id], user_id: @question.user.id)
+post '/questions/:question_id/answers' do
+  @question = Question.find_by(id: params[:question_id])
+  @answer = Answer.new(body: params[:body], question_id: params[:question_id], user_id: @question.user.id)
 
   if @answer.save
     redirect "/questions/#{@question.id}"
   else
     @errors = @answer.errors.full_messages
+  end
+end
+
+get '/questions/:question_id/answers/:answer_id/edit' do
+  @question = Question.find_by(id: params[:question_id])
+  @answer = Answer.find_by(id: params[:answer_id])
+  erb :'/answers/edit'
+end
+
+put '/questions/:question_id/answers/:answer_id' do
+  @question = Question.find_by(id: params[:question_id])
+  @answer = Answer.find_by(id: params[:answer_id])
+  if @answer.update(body: params[:body])
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @answer.errors
+    erb :'/answers/edit'
   end
 end
